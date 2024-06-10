0ad9c26690a3d7a05fa254b26c2f79199555d84c
MitcovaKate <katya.mitkova@bk.ru>
2024-06-11 01:02:51 +0300

    button onclick

--- src/scene/flake/Flake.js
+++ src/scene/flake/Flake.js
@@ -1,16 +1,27 @@
-import "./Flake.css"
-
+import "./Flake.css";
+import { randomCoordinate } from "../utils/coords"
 const Flake=()=>{
+
+    let top=0
     const style ={
-        top:'0%',
-        left:'10%'
+        top:`${top}%`,
+        left:`${randomCoordinate()}%`
     }
     return(
+        <>
         <div className="flake"
         style={style}
         >
 
         </div>
+        <button 
+        onClick={()=>{
+          top++
+          console.log(top)
+        }}>
+         DOWN
+        </button>
+        </>
     )
 }
 export  {Flake}
---
+++ src/utils/coords.js
@@ -0,0 +1,4 @@
+const randomCoordinate=()=>{
+    Math.random() * 100
+}
+export {randomCoordinate}
